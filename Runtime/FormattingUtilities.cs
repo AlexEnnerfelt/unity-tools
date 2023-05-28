13dbc3b1ef8d917df15af017fcc209f3bc28eaee
Alex Ennerfelt <alex.ennerfelt@gmail.com>
2023-05-28 16:33:19 +0800

    feature: added some utiliteies for inline formatting strings for rich text

---
+++ Runtime/FormattingUtilities.cs
@@ -0,0 +1,28 @@
+public static class FormattiningUtilities
+{
+    public const string black = "<color=black>";
+    public const string blue = "<color=blue>";
+    public const string brown = "<color=brown>";
+    public const string cyan = "<color=cyan>";
+    public const string darkBlue = "<color=darkblue>";
+    public const string green = "<color=green>";
+    public const string grey = "<color=grey>";
+    public const string lightBlue = "<color=lightblue>";
+    public const string lime = "<color=lime>";
+    public const string magenta = "<color=magenta>";
+    public const string maroon = "<color=maroon>";
+    public const string navy = "<color=navy>";
+    public const string olive = "<color=olive>";
+    public const string orange = "<color=orange>";
+    public const string purple = "<color=purple>";
+    public const string red = "<color=red>";
+    public const string silver = "<color=silver>";
+    public const string teal = "<color=teal>";
+    public const string white = "<color=white>";
+    public const string yellow = "<color=yellow>";
+    public const string cEnd = "</color>";
+    public const string b = "<b>";
+    public const string bEnd = "</b>";
+    public const string i = "<i>";
+    public const string iEnd = "</i>";
+}
