d9538783ee25581510fea13bb3a0f215c0d2d84e
Alex Ennerfelt <alex.ennerfelt@gmail.com>
2023-11-22 14:37:39 +0800

    feature: added restrict attribute (+1 squashed commits)
    Squashed commits:
    
    [ab9b64a] statretd work on a restrict attribute

---
+++ Runtime/Attributes/RestrictAttribute.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Attribute that require implementation of the provided interface.
+/// </summary>
+public class RestrictAttribute : PropertyAttribute {
+	// Interface type.
+	public System.Type requiredType { get; private set; }
+
+	/// <summary>
+	/// Requiring implementation of the <see cref="T:RequireInterfaceAttribute"/> interface.
+	/// </summary>
+	/// <param name="type">Interface type.</param>
+	public RestrictAttribute(System.Type type) {
+		this.requiredType = type;
+	}
+}
