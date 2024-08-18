80972a3deb82c7ddf86666d85b7dc0a9c64fe8c0
Alex Ennerfelt <alex.ennerfelt@gmail.com>
2024-08-18 16:16:44 +0800

    feature: added netcode extensions for easily checkinf if local client can read or write networkvariable

---
+++ Runtime/NetcodeComponents/NetcodeUtilities.cs
@@ -0,0 +1,12 @@
+﻿using Unity.Netcode;
+
+namespace UnpopularOpinion.Tools {
+    public static class NetcodeUtilities {
+        public static bool CanIWrite(this NetworkVariableBase netVar) {
+            return netVar.CanClientWrite(NetworkManager.Singleton.LocalClientId);
+        }
+        public static bool CanIRead(this NetworkVariableBase netVar) {
+            return netVar.CanClientRead(NetworkManager.Singleton.LocalClientId);
+        }
+    }
+}
