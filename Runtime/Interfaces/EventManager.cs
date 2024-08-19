c0a6a9c8dbccb309e676dd51970e05df55cd7469
Alex Ennerfelt <alex.ennerfelt@gmail.com>
2024-08-19 20:59:54 +0800

    refactor: removed IStaticReferable

--- Runtime/Interfaces/EventManager.cs
+++ Runtime/Interfaces/EventManager.cs
@@ -1,17 +1,17 @@
 using System;
 
 public class EventManager<T> {
-	public static T _event { get; protected set; }
-	public static event Action<T> onraised;
+    public static T _event { get; protected set; }
+    public static event Action<T> onraised;
 
-	public static void AddListener(Action<T> listener) {
-		onraised += listener;
-	}
-	public static void RemoveListener(Action<T> listener) {
-		onraised -= listener;
-	}
-	public static void Trigger(T arg) {
-		_event = arg;
-		onraised?.Invoke(_event);
-	}
+    public static void AddListener(Action<T> listener) {
+        onraised += listener;
+    }
+    public static void RemoveListener(Action<T> listener) {
+        onraised -= listener;
+    }
+    public static void Trigger(T arg) {
+        _event = arg;
+        onraised?.Invoke(_event);
+    }
 }
